67b6fdf3506ff0371e2e44ed72308aac97da629d
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 17:20:16 +0800

    make an elif statement

--- exercise 12.py
+++ exercise 12.py
@@ -6,3 +6,8 @@ def tax(income):
     if income < 10000:
         tax_to_pay = 0
         print("There's no income tax you need to pay.")
+    # make in elif statement
+    elif income > 10000 and income <= 20000:
+        tax_to_pay = (income - 10000) * 0.1
+        print(f"The income tax that you need to pay is ${tax_to_pay}")
+ 
